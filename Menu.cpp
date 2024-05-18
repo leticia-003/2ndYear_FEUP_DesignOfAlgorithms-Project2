731d3eb71bf6272122453e57cf911439d96587b0
Letícia Coelho <leticiamendes2122@gmail.com>
2024-05-18 01:21:08 +0100

    able to perform more algorithms in one session

--- Menu.cpp
+++ Menu.cpp
@@ -77,155 +77,171 @@ std::string getDatasetChoice() {
 
         Algorithms algo(&toyGraph);
 
-        std::cout << std::endl;
-        std::cout << "+----------------------------------------+" << std::endl;
-        std::cout << "|          Choose an action              |" << std::endl;
-        std::cout << "+----------------------------------------+" << std::endl;
-        std::cout << "|  1. Print the graph                    |" << std::endl;
-        std::cout << "|  2. TSP Backtracking                   |" << std::endl;
-        std::cout << "|  3. Triangular Approximation Heuristic |" << std::endl;
-        std::cout << "|  4. Nearest Neighbor/ 2-OPT Heuristic  |" << std::endl;
-        std::cout << "|  5. MST Prim                           |" << std::endl;
-        std::cout << "+----------------------------------------+" << std::endl;
+        bool continueLoop = true;
+        while (continueLoop) {
 
-        int actionChoice;
-        std::cout << "Enter your choice (1, 2, 3, 4 or 5): ";
-        std::cin >> actionChoice;
+            std::cout << std::endl;
+            std::cout << "+----------------------------------------+" << std::endl;
+            std::cout << "|          Choose an action              |" << std::endl;
+            std::cout << "+----------------------------------------+" << std::endl;
+            std::cout << "|  1. Print the graph                    |" << std::endl;
+            std::cout << "|  2. TSP Backtracking                   |" << std::endl;
+            std::cout << "|  3. Triangular Approximation Heuristic |" << std::endl;
+            std::cout << "|  4. Nearest Neighbor/ 2-OPT Heuristic  |" << std::endl;
+            std::cout << "|  5. MST Prim                           |" << std::endl;
+            std::cout << "|                                        |" << std::endl;
+            std::cout << "|  6. Exit                               |" << std::endl;
+            std::cout << "+----------------------------------------+" << std::endl;
 
-        std::vector<int> bestPath;
+            int actionChoice;
+            std::cout << "Enter your choice (1, 2, 3, 4, 5 or 6): ";
+            std::cin >> actionChoice;
 
-        switch (actionChoice) {
-            case 1:
-                // Print the graph
-                //graphHandler.printGraph(toyGraph);
-                break;
-            case 2:
-                // Perform TSP backtracking
-                algo.backtrackingAlgorithm(toyGraph, graphFiles[graphChoice - 1]);
-                break;
-            case 3:
-                if (graphChoice == 3) {
-                    std::cout << "The graph is not fully connected. Unable to apply the Triangular Approximation Heuristic." << std::endl;
-                } else {
-                    auto startTime = std::chrono::high_resolution_clock::now();
-                    int startId = 0;  // Or whichever vertex you want to start from
-                    std::vector<int> tspPath;
-                    double tspCost = algo.tsp2Approximation(startId, tspPath);
+            std::vector<int> bestPath;
 
-                    std::cout << "TSP Path: ";
-                    for (size_t i = 0; i < tspPath.size(); ++i) {
-                        std::cout << tspPath[i];
-                        if (i < tspPath.size() - 1) {
-                            std::cout << " -> ";
+            switch (actionChoice) {
+                case 1:
+                    // Print the graph
+                    //graphHandler.printGraph(toyGraph);
+                    break;
+                case 2:
+                    // Perform TSP backtracking
+                    algo.backtrackingAlgorithm(toyGraph, graphFiles[graphChoice - 1]);
+                    break;
+                case 3:
+                    if (graphChoice == 3) {
+                        std::cout
+                                << "The graph is not fully connected. Unable to apply the Triangular Approximation Heuristic."
+                                << std::endl;
+                    } else {
+                        auto startTime = std::chrono::high_resolution_clock::now();
+                        int startId = 0;  // Or whichever vertex you want to start from
+                        std::vector<int> tspPath;
+                        double tspCost = algo.tsp2Approximation(startId, tspPath);
+
+                        std::cout << "TSP Path: ";
+                        for (size_t i = 0; i < tspPath.size(); ++i) {
+                            std::cout << tspPath[i];
+                            if (i < tspPath.size() - 1) {
+                                std::cout << " -> ";
+                            }
                         }
+                        std::cout << std::endl;
+
+                        std::cout << "Total Cost of TSP Path: " << tspCost << std::endl;
+                        auto endTime = std::chrono::high_resolution_clock::now();
+                        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                                endTime - startTime).count();
+                        std::cout << "Time taken: " << duration << " ms" << std::endl;
                     }
+                    break;
+
+                case 4:
+                    if (graphChoice == 3) {
+                        std::cout
+                                << "The graph is not fully connected. Unable to apply the Triangular Approximation Heuristic."
+                                << std::endl;
+                    } else {
+                        int startNode = 0;
+                        auto startTimeNN = std::chrono::high_resolution_clock::now();
+                        auto nnResult = algo.nearestNeighbor(toyGraph, startNode);
+                        auto endTimeNN = std::chrono::high_resolution_clock::now();
+                        auto durationNN = std::chrono::duration_cast<std::chrono::milliseconds>(
+                                endTimeNN - startTimeNN).count();
+
+                        double nnCost = nnResult.first;
+                        std::vector<int> nnTour = nnResult.second;
+
+                        std::cout << std::endl;
+                        std::cout << "+----------------------------------------+" << std::endl;
+                        std::cout << "|    Nearest Neighbor/2-OPT Heuristic    |" << std::endl;
+                        std::cout << "+----------------------------------------+" << std::endl;
+                        std::cout << "| Nearest Neighbor Tour: ";
+                        algo.printTour(nnTour);
+                        std::cout << "| Nearest Neighbor Tour Cost: " << nnCost << std::endl;
+                        std::cout << "| Time taken: " << durationNN << " ms" << std::endl;
+                        std::cout << std::endl;
+                        std::cout << "+----------------------------------------+" << std::endl;
+                        std::cout << std::endl;
+
+                        auto startTime2OPT = std::chrono::high_resolution_clock::now();
+                        double TwoOptCost = algo.tSP2OptImprovement(toyGraph, nnTour);
+                        auto endTime2OPT = std::chrono::high_resolution_clock::now();
+                        auto duration2OPT = std::chrono::duration_cast<std::chrono::milliseconds>(
+                                endTime2OPT - startTime2OPT).count();
+                        std::cout << "| 2-OPT Tour Cost: " << TwoOptCost << std::endl;
+                        std::cout << "| Time taken: " << duration2OPT << " ms" << std::endl;
+                        std::cout << std::endl;
+                        std::cout << "+----------------------------------------+" << std::endl;
+
+                        double percentage = round(((nnCost - TwoOptCost) * 100) / nnCost);
+                        std::cout << "|    Improvement Percentage:    " << percentage << "%       |" << std::endl;
+                        std::cout << "+----------------------------------------+" << std::endl;
+                    }
+                    break;
+
+                case 5: {
+                    auto startTime = std::chrono::high_resolution_clock::now();
+                    int startId; // Starting vertex ID is fixed at 0
+                    std::cout << "Write a starting point (id): ";
+                    std::cin >> startId;
+
+                    // Call mstPrim function
+                    std::vector<std::pair<unsigned, unsigned>> mST;
+                    double mstCost = toyGraph.mstPrim(startId, mST);
+
+                    // Output the cost of the Minimum Spanning Tree
+                    std::cout << "The cost of the Minimum Spanning Tree is: " << mstCost << std::endl;
+
+                    // Output the path of the Minimum Spanning Tree
+                    std::cout << "The path of the Minimum Spanning Tree is: ";
+
+                    if (!mST.empty()) {
+                        // Create a map to track the adjacency list
+                        std::map<unsigned, std::vector<unsigned>> adjList;
+                        for (const auto &edge: mST) {
+                            adjList[edge.first].push_back(edge.second);
+                            adjList[edge.second].push_back(edge.first);
+                        }
+
+                        // Perform a BFS or DFS to get the path from the start vertex
+                        std::vector<unsigned> path;
+                        std::set<unsigned> visited;
+                        std::function<void(unsigned)> dfs = [&](unsigned node) {
+                            visited.insert(node);
+                            path.push_back(node);
+                            for (unsigned neighbor: adjList[node]) {
+                                if (visited.find(neighbor) == visited.end()) {
+                                    dfs(neighbor);
+                                }
+                            }
+                        };
+
+                        dfs(startId);
+
+                        for (size_t i = 0; i < path.size(); ++i) {
+                            std::cout << path[i] << " -> ";
+                        }
+                        std::cout << startId;
+                    } else {
+                        std::cout << "No path found.";
+                    }
+
                     std::cout << std::endl;
 
-                    std::cout << "Total Cost of TSP Path: " << tspCost << std::endl;
                     auto endTime = std::chrono::high_resolution_clock::now();
                     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
                     std::cout << "Time taken: " << duration << " ms" << std::endl;
+                    break;
                 }
-                break;
 
-            case 4:
-                if (graphChoice == 3) {
-                    std::cout << "The graph is not fully connected. Unable to apply the Triangular Approximation Heuristic." << std::endl;
-                }
-                else {
-                int startNode = 0;
-                auto startTimeNN = std::chrono::high_resolution_clock::now();
-                auto nnResult = algo.nearestNeighbor(toyGraph, startNode);
-                auto endTimeNN = std::chrono::high_resolution_clock::now();
-                auto durationNN = std::chrono::duration_cast<std::chrono::milliseconds>(endTimeNN - startTimeNN).count();
+                case 6:
+                    continueLoop = false;
+                    break;
 
-                double nnCost = nnResult.first;
-                std::vector<int> nnTour = nnResult.second;
-
-                std::cout << std::endl;
-                std::cout << "+----------------------------------------+" << std::endl;
-                std::cout << "|    Nearest Neighbor/2-OPT Heuristic    |" << std::endl;
-                std::cout << "+----------------------------------------+" << std::endl;
-                std::cout << "| Nearest Neighbor Tour: ";
-                algo.printTour(nnTour);
-                std::cout << "| Nearest Neighbor Tour Cost: " << nnCost << std::endl;
-                std::cout << "| Time taken: " << durationNN << " ms" << std::endl;
-                std::cout << std::endl;
-                std::cout << "+----------------------------------------+" << std::endl;
-                std::cout << std::endl;
-
-                auto startTime2OPT = std::chrono::high_resolution_clock::now();
-                double TwoOptCost = algo.tSP2OptImprovement(toyGraph, nnTour);
-                auto endTime2OPT = std::chrono::high_resolution_clock::now();
-                auto duration2OPT = std::chrono::duration_cast<std::chrono::milliseconds>(endTime2OPT - startTime2OPT).count();
-                std::cout << "| 2-OPT Tour Cost: " << TwoOptCost << std::endl;
-                std::cout << "| Time taken: " << duration2OPT << " ms" << std::endl;
-                std::cout << std::endl;
-                std::cout << "+----------------------------------------+" << std::endl;
-
-                double percentage = round(((nnCost - TwoOptCost) * 100) / nnCost);
-                std::cout << "|    Improvement Percentage:    " << percentage << "%       |" << std::endl;
-                std::cout << "+----------------------------------------+" << std::endl;
+                default:
+                    std::cout << "Invalid choice" << std::endl;
             }
-                break;
-
-            case 5: {
-                auto startTime = std::chrono::high_resolution_clock::now();
-                int startId; // Starting vertex ID is fixed at 0
-                std::cout << "Write a starting point (id): ";
-                std::cin >> startId;
-
-                // Call mstPrim function
-                std::vector<std::pair<unsigned, unsigned>> mST;
-                double mstCost = toyGraph.mstPrim(startId, mST);
-
-                // Output the cost of the Minimum Spanning Tree
-                std::cout << "The cost of the Minimum Spanning Tree is: " << mstCost << std::endl;
-
-                // Output the path of the Minimum Spanning Tree
-                std::cout << "The path of the Minimum Spanning Tree is: ";
-
-                if (!mST.empty()) {
-                    // Create a map to track the adjacency list
-                    std::map<unsigned, std::vector<unsigned>> adjList;
-                    for (const auto& edge : mST) {
-                        adjList[edge.first].push_back(edge.second);
-                        adjList[edge.second].push_back(edge.first);
-                    }
-
-                    // Perform a BFS or DFS to get the path from the start vertex
-                    std::vector<unsigned> path;
-                    std::set<unsigned> visited;
-                    std::function<void(unsigned)> dfs = [&](unsigned node) {
-                        visited.insert(node);
-                        path.push_back(node);
-                        for (unsigned neighbor : adjList[node]) {
-                            if (visited.find(neighbor) == visited.end()) {
-                                dfs(neighbor);
-                            }
-                        }
-                    };
-
-                    dfs(startId);
-
-                    for (size_t i = 0; i < path.size(); ++i) {
-                        std::cout << path[i] << " -> ";
-                    }
-                    std::cout  << startId;
-                } else {
-                    std::cout << "No path found.";
-                }
-
-                std::cout << std::endl;
-
-                auto endTime = std::chrono::high_resolution_clock::now();
-                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
-                std::cout << "Time taken: " << duration << " ms" << std::endl;
-                break;
-            }
-
-            default:
-                std::cout << "Invalid choice" << std::endl;
         }
     }
 
